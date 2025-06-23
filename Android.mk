bd2f5646434afe2bbf3034be4e53ed8e5d2fbf0c
Michael Bestas <mkbestas@lineageos.org>
2025-06-23 19:50:27 +0300

    bluejay: Update to BP2A.250605.031.A2
    Change-Id: Iff741cb1fbc41826eedd6405d682ac3b16ff0ff8

--- Android.mk
+++ Android.mk
@@ -6,14 +6,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),bluejay)
 
-$(call add-radio-file-sha1-checked,radio/abl.img,c3a15da4035eff1c3b3bf862366ec395e7bad92a)
-$(call add-radio-file-sha1-checked,radio/bl1.img,8fa2826e525bd07636a6907defc3f87537e697ff)
-$(call add-radio-file-sha1-checked,radio/bl2.img,ac3335cbfc8ca381c8ac1891e5c1a59f76c41642)
-$(call add-radio-file-sha1-checked,radio/bl31.img,05435596bc66490dbb55f53cc7b5a30385d84c41)
-$(call add-radio-file-sha1-checked,radio/gsa.img,c0c2dde748a6a597d03d460f3568563069411ea7)
-$(call add-radio-file-sha1-checked,radio/ldfw.img,ca70485cf97f14101eeb9e0768f5f75d4e2e8eff)
-$(call add-radio-file-sha1-checked,radio/modem.img,6fcfff009d9460df259acf29d8f799dacb7d2d1a)
-$(call add-radio-file-sha1-checked,radio/pbl.img,c6087a899901f6c8da92c1c7bff47b4ae937919d)
-$(call add-radio-file-sha1-checked,radio/tzsw.img,437179dfc345d05eeac3dfcb70ba37f9950fde90)
+$(call add-radio-file-sha1-checked,radio/abl.img,382b67e4213415cee838f6a76fec0cb1a77f8a20)
+$(call add-radio-file-sha1-checked,radio/bl1.img,bf5a1977f6da6bb4292419ba45998f90ea10d1c3)
+$(call add-radio-file-sha1-checked,radio/bl2.img,a215e9c786aaff4179c201294816846d53a1b127)
+$(call add-radio-file-sha1-checked,radio/bl31.img,05fc2f9b1e7f1b333f1acd7c40f1093f4b54232c)
+$(call add-radio-file-sha1-checked,radio/gsa.img,4ecf12ecdc286fb6391eb79f5c6d431a2b6df37f)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,92bc23dcc0ec39437f09cf80ceffce0a072d979b)
+$(call add-radio-file-sha1-checked,radio/modem.img,3b130274941a093ddd21264c059464cd662e6a65)
+$(call add-radio-file-sha1-checked,radio/pbl.img,9860fe37cd546689ef08dfc32b6980d403d58432)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,7d7a178176453d54821a6dcca133cab8975dd3ef)
 
 endif
