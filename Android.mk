7643382450601ab62b688eaef4175e4cd5931e3e
Michael Bestas <mkbestas@lineageos.org>
2026-06-27 06:15:46 +0300

    bluejay: Update to CP2A.260605.012
    Change-Id: Ic174217325cd2e7035b7a142168d0bb73c587828

--- Android.mk
+++ Android.mk
@@ -6,14 +6,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),bluejay)
 
-$(call add-radio-file-sha1-checked,radio/abl.img,8cbaef935028ac4864c5052b0f06fcf4860c95e4)
-$(call add-radio-file-sha1-checked,radio/bl1.img,c17bd24e19c78010b8219884b800cf640ecf7d7c)
-$(call add-radio-file-sha1-checked,radio/bl2.img,81dfee8b18641c92e4ea4cc62f14c7950a4bcf22)
-$(call add-radio-file-sha1-checked,radio/bl31.img,bce2abc94dde3eda75d8a98963865439ee961fba)
-$(call add-radio-file-sha1-checked,radio/gsa.img,326455ae18474ce2a921bc6f30048caa05265b08)
-$(call add-radio-file-sha1-checked,radio/ldfw.img,15600d9811eca0d7a12cda8bbdd04ff2bac1f3e9)
-$(call add-radio-file-sha1-checked,radio/modem.img,714a4bcf40db4b91528be0ac7dd8942d62a51e68)
-$(call add-radio-file-sha1-checked,radio/pbl.img,c845f92492c09029b40d2ad4b4661a6b6661b0fa)
-$(call add-radio-file-sha1-checked,radio/tzsw.img,a1d5de783aa38f8ec89a6163072bf88338ad857a)
+$(call add-radio-file-sha1-checked,radio/abl.img,96f9649f02cdc0db8008bf74a002621317309946)
+$(call add-radio-file-sha1-checked,radio/bl1.img,da10210aa57453f7ccd87e81273f8aed5cb213b7)
+$(call add-radio-file-sha1-checked,radio/bl2.img,866bb513a5c6ce525147a348e65bdf732922aff6)
+$(call add-radio-file-sha1-checked,radio/bl31.img,c907120a997af6ab031aa7e50c16a18835b82c4a)
+$(call add-radio-file-sha1-checked,radio/gsa.img,76d42cf06b2fe46ee4168c74be53b43c57ce843b)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,7ff641cd086e97e625e072875c6f803c5db6889f)
+$(call add-radio-file-sha1-checked,radio/modem.img,372db2298f1b2400a6b647e10ba7ba6207b34e39)
+$(call add-radio-file-sha1-checked,radio/pbl.img,d2946c529aa00157ca4d97417574e1e0e313889e)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,6b3f49c4913dc4e370f49c03cbeb90d36e7b58c6)
 
 endif
